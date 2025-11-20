44bb86b4b3c7b925105068f6659bc715f683ddc7
Ahmed Fahmy <ahmedmewans2030@gmail.com>
2025-11-20 23:15:11 +0200

    feat(generate text): handle generate text from ai

---
+++ src/app/api/completion/route.ts
@@ -0,0 +1,17 @@
+import { groq } from "@ai-sdk/groq";
+import { generateText } from "ai";
+
+export async function POST(req: Request) {
+  try {
+    const { prompt } = await req.json();
+    const { text } = await generateText({
+      model: groq("llama-3.1-8b-instant"),
+      prompt: prompt,
+    });
+
+    return Response.json({ text });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Failed to generate text" });
+  }
+}
